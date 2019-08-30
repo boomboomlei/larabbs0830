65832b14ac87f95a558facf46b98323a977c1088
Your Nameboomboomlei <1846352100@qq.com>
2019-08-30 13:58:13 +0000

    认证后的消息提醒

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -7,35 +7,17 @@ use Illuminate\Foundation\Auth\VerifiesEmails;
 
 class VerificationController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Email Verification Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling email verification for any
-    | user that recently registered with the application. Emails may also
-    | be re-sent if the user didn't receive the original email message.
-    |
-    */
 
     use VerifiesEmails;
 
-    /**
-     * Where to redirect users after verification.
-     *
-     * @var string
-     */
+ 
     protected $redirectTo = '/';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'resend');
+        $this->middleware('auth');//有的控制器动作都需要登录后才能访问
+        $this->middleware('signed')->only('verify'); //设定了 只有 verify 动作使用 signed 中间件进行认证
+        $this->middleware('throttle:6,1')->only('verify', 'resend');//对 verify 和 resend 动作做了频率限制,访问频率是 1 分钟内不能超过 6 次。
     }
 }
