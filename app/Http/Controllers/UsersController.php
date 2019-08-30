ad6453e0d3cea0e34196e0d66dc4dd16796f36af
Your Nameboomboomlei <1846352100@qq.com>
2019-08-30 15:57:29 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 
+use App\Handlers\ImageUploadHandler;
+
 class UsersController extends Controller
 {
     public  function show(User $user){
@@ -15,8 +17,21 @@ class UsersController extends Controller
     	return view('users.edit',compact('user'));
     }
 
-    public  function update(UserRequest $request,User $user){
-    	$user->update($request->all());
+    public  function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
+    	// $user->update($request->all());
+   
+    	$data=$request->all();
+
+    	if($request->avatar){
+    		$result=$uploader->save($request->avatar,'avatars',$user->id);
+    		if($result){
+    			$data['avatar']=$result['path'];
+    		}
+    	}
+
+    	$user->update($data);
+
+
     	return redirect()->route('users.show',$user->id)->with('success', '个人资料更新成功！');
     }
 }
