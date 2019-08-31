f7094de326e1ba8ff3b70cf562c0eb42fbf5f648
Your Nameboomboomlei <1846352100@qq.com>
2019-08-31 09:14:01 +0000

    填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        $faker=app(Faker\Generator::class);
+
+         // 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        $users=factory(User::class)
+        			->times(10)
+        			->make()
+        			->each(function ($user,$index) use ($faker,$avatars){
+        				$user->avatar=$faker->randomElement($avatars);
+        			});
+
+        $user_array=$users->makeVisible(['password','remember_token'])->toArray();
+
+        User::insert($user_array);
+
+        $user=User::find(1);
+        $user->name="Summer";
+        $user->email='summer@example.com';
+        $user->avatar="https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png";
+        $user->save();
+
+    }
+}
