653c3fe626c9596333ae16b104ece1e6cdd2f37a
Your Nameboomboomlei <1846352100@qq.com>
2019-08-31 09:47:23 +0000

    修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,8 +16,10 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
-		return view('topics.index', compact('topics'));
+		// $topics = Topic::paginate(30);
+		// return view('topics.index', compact('topics'));
+		$topics=Topic::with('user','category')->paginate(30);
+		return view('topics.index',compact('topics'));
 	}
 
     public function show(Topic $topic)
