69673eed4e4cf3b347bc99a354337da647a132e3
Your Nameboomboomlei <1846352100@qq.com>
2019-08-31 11:36:04 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+
+Route::resource('categories','CategoriesController',['only'=>['show']]);
