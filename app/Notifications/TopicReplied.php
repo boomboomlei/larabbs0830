7aae3ed796d76a211def17622068aa6106796332
Your Nameboomboomlei <1846352100@qq.com>
2019-09-02 08:47:14 +0000

    支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Reply;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -27,7 +27,7 @@ class TopicReplied extends Notification
      */
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database','mail'];
     }
 
     public function toDatabase($notifiable){
@@ -58,10 +58,9 @@ class TopicReplied extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+      $url=$this->reply->topic->link(['#reply'.$this->reply->id]);
+
+      return (new MailMessage)->line('小贱人你有新的回复！')->action('查看回复',$url);
     }
 
     /**
