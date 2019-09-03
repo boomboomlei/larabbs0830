164ff9e151d908608e29148dc127db250cf4247a
Your Nameboomboomlei <1846352100@qq.com>
2019-09-03 13:25:45 +0000

    管理后台权限

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,4 +9,13 @@ class PagesController extends Controller
     public  function root(){
     	return view('pages.root');
     }
+
+
+    public function permissionDenied(){
+    	if(config('administrator.permission')()){
+    		return  redirect(url(config('administrator.uri')),302);
+    	}
+
+    	return view('pages.permission_denied');
+    }
 }
