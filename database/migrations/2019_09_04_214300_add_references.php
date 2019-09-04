8d1447c07f6341ca2e53257fe48d40e02b932a70
Your Nameboomboomlei <1846352100@qq.com>
2019-09-04 13:52:54 +0000

    联动删除

---
+++ database/migrations/2019_09_04_214300_add_references.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddReferences extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('topics',function(Blueprint $table){
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::table('replies',function(Blueprint $table){
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        
+            $table->foreign('topic_id')->references('id')->on('topics')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('topics',function(Blueprint $table){
+
+            $table->dropForeign(['user_id']);
+
+        });
+        Schema::table('replies',function(Blueprint $table){
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['topic_id']);
+        });
+    }
+}
